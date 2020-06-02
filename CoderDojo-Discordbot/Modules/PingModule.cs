2dacbd83f177d6c4d3986e36b03f49b3d59c0b71
Jonas kamsker <jonaskamsker@hotmail.com>
2020-06-02 20:16:22 +0200

    Add project files.

---
+++ CoderDojo-Discordbot/Modules/PingModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace CoderDojo_Discordbot.Modules
+{
+    public class PingModule : ModuleBase
+    {
+        [Command("ping")]
+        public async Task TestCommandAsync()
+        {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
